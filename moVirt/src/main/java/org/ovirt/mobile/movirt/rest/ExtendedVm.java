39aeb38fd5c36732fa45934be29e6113d21c50c8
Sphoorti Joglekar <sphoorti.joglekar@gmail.com>
2015-01-28 19:10:51 +0530

    Enrich VmDetailActivity to show additional details.

--- moVirt/src/main/java/org/ovirt/mobile/movirt/rest/ExtendedVm.java
+++ moVirt/src/main/java/org/ovirt/mobile/movirt/rest/ExtendedVm.java
@@ -2,19 +2,29 @@ package org.ovirt.mobile.movirt.rest;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import org.ovirt.mobile.movirt.model.VmStatistics;
-
 /**
  * Additional parameters which are not going to be persisted
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ExtendedVm extends Vm {
 
+    public String memory;
     public Display display;
+    public Os os;
+    public Cpu cpu;
 
     @JsonIgnoreProperties(ignoreUnknown = true)
     public static class Display {
         public String address, port, type;
     }
 
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Os {
+        public String type;
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Cpu {
+        public Topology topology;
+    }
 }
