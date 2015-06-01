af60ac0b1540e9523a2becd60b82e9736e8151ba
NoiseDoll <bolya2003@bk.ru>
2015-06-01 21:47:25 +1000

    Now camera decodes DataMatrix codes and shows decoded text.

---
+++ moVirt/src/main/java/org/ovirt/mobile/movirt/camera/DecodeThread.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2008 ZXing authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.ovirt.mobile.movirt.camera;
+
+import android.os.Handler;
+import android.os.Looper;
+
+import org.ovirt.mobile.movirt.ui.CameraActivity;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * This thread does all the heavy lifting of decoding the images.
+ *
+ * @author dswitkin@google.com (Daniel Switkin)
+ */
+final class DecodeThread extends Thread {
+
+  public static final String BARCODE_BITMAP = "barcode_bitmap";
+  public static final String BARCODE_SCALED_FACTOR = "barcode_scaled_factor";
+
+  private final CameraActivity activity;
+  private Handler handler;
+  private final CountDownLatch handlerInitLatch;
+
+  DecodeThread(CameraActivity activity) {
+
+    this.activity = activity;
+    handlerInitLatch = new CountDownLatch(1);
+  }
+
+  Handler getHandler() {
+    try {
+      handlerInitLatch.await();
+    } catch (InterruptedException ie) {
+      // continue?
+    }
+    return handler;
+  }
+
+  @Override
+  public void run() {
+    Looper.prepare();
+    handler = new DecodeHandler(activity);
+    handlerInitLatch.countDown();
+    Looper.loop();
+  }
+
+}
--- moVirt/src/main/java/org/ovirt/mobile/movirt/ui/CameraActivity.java
+++ moVirt/src/main/java/org/ovirt/mobile/movirt/ui/CameraActivity.java
@@ -1,26 +1,68 @@
 package org.ovirt.mobile.movirt.ui;
 
 import android.app.Activity;
-import android.content.Context;
-import android.hardware.Camera;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.widget.FrameLayout;
-import android.widget.Toast;
+import android.os.Handler;
+import android.os.Message;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 import org.ovirt.mobile.movirt.R;
-import org.ovirt.mobile.movirt.camera.CameraPreview;
+import org.ovirt.mobile.movirt.camera.CaptureActivityHandler;
+import org.ovirt.mobile.movirt.camera.zxing.Result;
+import org.ovirt.mobile.movirt.camera.zxing.client.CameraManager;
+import org.ovirt.mobile.movirt.camera.zxing.client.PreferencesActivity;
 
-public class CameraActivity extends Activity {
+import java.io.IOException;
 
-    private Camera mCamera;
-    private CameraPreview mPreview;
+public final class CameraActivity extends Activity implements SurfaceHolder.Callback {
+
+    private static final String TAG = CameraActivity.class.getSimpleName();
+    private static final long BULK_MODE_SCAN_DELAY_MS = 1000L;
+
+    private CameraManager cameraManager;
+    private CaptureActivityHandler handler;
+    //private ViewfinderView viewfinderView;
+    private boolean hasSurface;
+    private Result savedResultToShow;
+
+    public CameraManager getCameraManager() {
+        return cameraManager;
+    }
+
+    /*ViewfinderView getViewfinderView() {
+        return viewfinderView;
+    }*/
+
+    public Handler getHandler() {
+        return handler;
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.activity_camera);
 
-        // Create an instance of Camera
+        hasSurface = false;
+/*      inactivityTimer = new InactivityTimer(this);
+        beepManager = new BeepManager(this);
+        ambientLightManager = new AmbientLightManager(this);*/
+
+        PreferenceManager.setDefaultValues(this, R.xml.zxing_preferences, false);
+
+/*      // Create an instance of Camera
         mCamera = getCameraInstance(CameraActivity.this);
 
         if(mCamera == null)
@@ -32,24 +74,155 @@ public class CameraActivity extends Activity {
             mPreview = new CameraPreview(this, mCamera);
             FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
             preview.addView(mPreview);
+        }*/
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // CameraManager must be initialized here, not in onCreate(). This is necessary because we don't
+        // want to open the camera driver and measure the screen size if we're going to show the help on
+        // first launch. That led to bugs where the scanning rectangle was the wrong size and partially
+        // off screen.
+        cameraManager = new CameraManager(getApplication());
+
+//        viewfinderView = (ViewfinderView) findViewById(R.id.viewfinder_view);
+//        viewfinderView.setCameraManager(cameraManager);
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        if (prefs.getBoolean(PreferencesActivity.KEY_DISABLE_AUTO_ORIENTATION, true)) {
+            setRequestedOrientation(getCurrentOrientation());
+        } else {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE);
+        }
+        SurfaceView surfaceView = (SurfaceView) findViewById(R.id.camera_preview);
+        SurfaceHolder surfaceHolder = surfaceView.getHolder();
+        if (hasSurface) {
+            // The activity was paused but not stopped, so the surface still exists. Therefore
+            // surfaceCreated() won't be called, so init the camera here.
+            initCamera(surfaceHolder);
+        } else {
+            // Install the callback and wait for surfaceCreated() to init the camera.
+            surfaceHolder.addCallback(this);
         }
     }
 
     @Override
-    public void onDestroy() {
-        super.onDestroy();
-        if (mCamera != null) mCamera.release();
+    protected void onPause() {
+        if (handler != null) {
+            handler.quitSynchronously();
+            handler = null;
+        }
+//        inactivityTimer.onPause();
+//        ambientLightManager.stop();
+//        beepManager.close();
+        cameraManager.closeDriver();
+        //historyManager = null; // Keep for onActivityResult
+        if (!hasSurface) {
+            SurfaceView surfaceView = (SurfaceView) findViewById(R.id.camera_preview);
+            SurfaceHolder surfaceHolder = surfaceView.getHolder();
+            surfaceHolder.removeCallback(this);
+        }
+        super.onPause();
     }
 
-    public static Camera getCameraInstance(Context context){
-        Camera c = null;
+    @Override
+    protected void onDestroy() {
+        //inactivityTimer.shutdown();
+        super.onDestroy();
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        if (holder == null) {
+            Log.e(TAG, "*** WARNING *** surfaceCreated() gave us a null surface!");
+        }
+        if (!hasSurface) {
+            hasSurface = true;
+            initCamera(holder);
+        }
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        hasSurface = false;
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    private void initCamera(SurfaceHolder surfaceHolder) {
+        if (surfaceHolder == null) {
+            throw new IllegalStateException("No SurfaceHolder provided");
+        }
+        if (cameraManager.isOpen()) {
+            Log.w(TAG, "initCamera() while already open -- late SurfaceView callback?");
+            return;
+        }
         try {
-            c = Camera.open(); // attempt to get a Camera instance
+            cameraManager.openDriver(surfaceHolder);
+            // Creating the handler starts the preview, which can also throw a RuntimeException.
+            if (handler == null) {
+                handler = new CaptureActivityHandler(this, cameraManager);
+            }
+            decodeOrStoreSavedBitmap(null, null);
+        } catch (IOException ioe) {
+            Log.w(TAG, ioe);
+            //displayFrameworkBugMessageAndExit();
+        } catch (RuntimeException e) {
+            // Barcode Scanner has seen crashes in the wild of this variety:
+            // java.?lang.?RuntimeException: Fail to connect to camera service
+            Log.w(TAG, "Unexpected error initializing camera", e);
+            //displayFrameworkBugMessageAndExit();
         }
-        catch (Exception e){
-            Toast.makeText(context, e.getMessage(), Toast.LENGTH_LONG).show();
-            // Camera is not available (in use or does not exist)
+    }
+
+    /**
+     * A valid barcode has been found, so give an indication of success and show the results.
+     *
+     * @param rawResult The contents of the barcode.
+     * @param scaleFactor amount by which thumbnail was scaled
+     * @param barcode   A greyscale bitmap of the camera data which was decoded.
+     */
+    public void handleDecode(Result rawResult, Bitmap barcode, float scaleFactor) {
+        TextView tw = (TextView)findViewById(R.id.result_text);
+        tw.setText(rawResult.getText());
+        restartPreviewAfterDelay(BULK_MODE_SCAN_DELAY_MS);
+    }
+
+    public void restartPreviewAfterDelay(long delayMS) {
+        if (handler != null) {
+            handler.sendEmptyMessageDelayed(R.id.restart_preview, delayMS);
         }
-        return c; // returns null if camera is unavailable
+        //resetStatusView();
+    }
+
+    private void decodeOrStoreSavedBitmap(Bitmap bitmap, Result result) {
+        // Bitmap isn't used yet -- will be used soon
+        if (handler == null) {
+            savedResultToShow = result;
+        } else {
+            if (result != null) {
+                savedResultToShow = result;
+            }
+            if (savedResultToShow != null) {
+                Message message = Message.obtain(handler, R.id.decode_succeeded, savedResultToShow);
+                handler.sendMessage(message);
+            }
+            savedResultToShow = null;
+        }
+    }
+
+    private int getCurrentOrientation() {
+        int rotation = getWindowManager().getDefaultDisplay().getRotation();
+        switch (rotation) {
+            case Surface.ROTATION_0:
+            case Surface.ROTATION_90:
+                return ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
+            default:
+                return ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE;
+        }
     }
 }
