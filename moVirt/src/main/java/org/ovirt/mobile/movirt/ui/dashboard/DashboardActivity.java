9c6a76829f6f248d16cad081f986a1011bd9fb76
suomiy <suomiy@gmail.com>
2016-01-08 19:58:49 +0100

    added up button

--- moVirt/src/main/java/org/ovirt/mobile/movirt/ui/dashboard/DashboardActivity.java
+++ moVirt/src/main/java/org/ovirt/mobile/movirt/ui/dashboard/DashboardActivity.java
@@ -1,12 +1,18 @@
 package org.ovirt.mobile.movirt.ui.dashboard;
 
+import android.content.Intent;
+import android.view.MenuItem;
 import android.view.WindowManager;
 import android.widget.ProgressBar;
 
 import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.App;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.OptionsItem;
 import org.androidannotations.annotations.ViewById;
+import org.ovirt.mobile.movirt.MoVirtApp;
 import org.ovirt.mobile.movirt.R;
+import org.ovirt.mobile.movirt.ui.MainActivity_;
 import org.ovirt.mobile.movirt.ui.MovirtActivity;
 
 @EActivity(R.layout.activity_dashboard)
@@ -16,6 +22,9 @@ public class DashboardActivity extends MovirtActivity {
     @ViewById
     ProgressBar progress;
 
+    @App
+    MoVirtApp app;
+
     @AfterViews
     void init() {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
@@ -23,4 +32,8 @@ public class DashboardActivity extends MovirtActivity {
         setProgressBar(progress);
     }
 
+    @OptionsItem(android.R.id.home)
+    public void homeSelected() {
+        app.startMainActivity();
+    }
 }
